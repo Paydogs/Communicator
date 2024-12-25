ad532b30c546d4fcd509f7b553e0f7f0df58f50a
Andras Olah <paydogs@gmail.com>
2024-12-25 01:05:26 +0100

    Initial commit

---
+++ Sources/Communicator/Client.swift
@@ -0,0 +1,21 @@
+//
+//  Client.swift
+//  Communicator
+//
+//  Created by Andras Olah on 2024. 12. 22..
+//
+
+import Foundation
+import Combine
+import CommunicatorCore
+
+public class Client: CommunicatorCore.Client, @unchecked Sendable {
+    public let logMessages = PassthroughSubject<String, Never>()
+
+    public override func debugLog(_ message: String) {
+        logMessages.send("\(message)")
+        #if DEBUG
+            print("[DEBUG] \(message)")
+        #endif
+    }
+}
